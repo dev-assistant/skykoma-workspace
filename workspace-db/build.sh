174e65b5f92acb6e822bbb5911bef4066dc4d403
956237586 <master@hylstudio.cn>
2024-09-29 15:10:35 +0800

    update workspace-db

--- workspace-db/build.sh
+++ workspace-db/build.sh
@@ -1,5 +1,7 @@
-export BUILDKIT_HOST=tcp://10.110.190.102:1234
-TAG=2024031605
-#CACHE_OPTS="--no-cache"
-sudo nerdctl --namespace k8s.io build $CACHE_OPTS -f DockerFile --buildkit-host $BUILDKIT_HOST \
-	-t registry.cn-hangzhou.aliyuncs.com/hylstudio/workspace:$TAG .
+TAG=2024031605-workspace-db
+REGISTRY=registry.cn-hangzhou.aliyuncs.com/hylstudio/workspace
+# export BUILDKIT_HOST=tcp://10.110.190.102:1234
+# CACHE_OPTS="--no-cache"
+# sudo nerdctl --namespace k8s.io build $CACHE_OPTS -f DockerFile --buildkit-host $BUILDKIT_HOST \
+	# -t $REGISTRY:$TAG .
+doceker build $CACHE_OPTS -f DockerFile -t REGISTRY:$TAG .
