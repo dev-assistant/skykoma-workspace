0badc5be169163f150b484bc0de316351306759f
956237586 <master@hylstudio.cn>
2024-09-29 13:42:23 +0800

    add opts

--- workspace-ubuntu/build.sh
+++ workspace-ubuntu/build.sh
@@ -3,5 +3,6 @@ TAG=2024092901
 REGISTRY=registry.hylstudio.local/skykoma-webide
 downloadUrl=http://192.168.0.3:5004/ideaIC-2023.1.5.tar.gz
 projectorServerUrl=http://192.168.0.3:5004/projector-server-v1.8.1.9.zip
-#--no-cache --progress=plain 
-docker build -f Dockerfile --build-arg downloadUrl=$downloadUrl --build-arg projectorServerUrl=$projectorServerUrl -t $REGISTRY:$TAG .
+# CACHE_OPTS="--no-cache"
+# PROGRESS_OPTS="--progress=plain"
+docker build $CACHE_OPTS -f Dockerfile --build-arg downloadUrl=$downloadUrl --build-arg projectorServerUrl=$projectorServerUrl -t $REGISTRY:$TAG .
