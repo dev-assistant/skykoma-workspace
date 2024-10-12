c36ec0fef7054d109ec653beb812a5818605b2ac
956237586 <master@hylstudio.cn>
2024-10-12 19:59:36 +0800

    update build.sh

--- workspace-ubuntu/build.sh
+++ workspace-ubuntu/build.sh
@@ -1,14 +1,15 @@
-TAG=2024100503
-REGISTRY=registry.hylstudio.local/skykoma-workspace
-downloadUrl=http://192.168.0.3:5004/ideaIC-2023.1.5.tar.gz
-projectorServerUrl=http://192.168.0.3:5004/projector-server-v1.8.1.12.zip
+TAG=2024101201-skykoma-workspace-test
+REGISTRY=registry.cn-hangzhou.aliyuncs.com/hylstudio/workspace
+downloadUrl=https://download.jetbrains.com/idea/ideaIC-2023.1.5.tar.gz
+projectorServerUrl=https://github.com/956237586/projector-server/releases/download/v1.8.1.12/projector-server-v1.8.1.12.zip
 # CACHE_OPTS="--no-cache"
 # PROGRESS_OPTS="--progress=plain"
 docker build $CACHE_OPTS -f Dockerfile --build-arg downloadUrl=$downloadUrl --build-arg projectorServerUrl=$projectorServerUrl -t $REGISTRY:$TAG .
 docker push $REGISTRY:$TAG
 exit 0
-# ali cloud mirrors
-ALI_REGISTRY=registry.cn-hangzhou.aliyuncs.com/hylstudio/workspace
-ALI_TAG=$TAG-skykoma-workspace
-docker tag $REGISTRY:$TAG $ALI_REGISTRY:$ALI_TAG
-docker push $ALI_REGISTRY:$ALI_TAG
+#local mirrors
+downloadUrl=http://192.168.0.3:5004/ideaIC-2023.1.5.tar.gz
+projectorServerUrl=http://192.168.0.3:5004/projector-server-v1.8.1.12.zip
+LOCAL_REGISTRY=registry.hylstudio.local/skykoma-workspace
+docker tag $REGISTRY:$TAG $LOCAL_REGISTRY:$TAG
+docker push $LOCAL_REGISTRY:$TAG
