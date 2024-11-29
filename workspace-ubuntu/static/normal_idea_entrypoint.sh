008c5ff613c4267c9b1e733ec8fa47894e6bef0a
HuangYongliang <master@hylstudio.cn>
2024-11-29 17:25:51 +0800

    Update normal_idea_entrypoint.sh

--- workspace-ubuntu/static/normal_idea_entrypoint.sh
+++ workspace-ubuntu/static/normal_idea_entrypoint.sh
@@ -3,7 +3,7 @@
 PGREP="com.intellij.idea.Main"
 while true
 do
-    echo -n 1 > /tmp/autorestart && pkill -f $PGREP
+    echo -n 1 > /tmp/autorestart && echo -n "raw" > /tmp/ideamode && pkill -f $PGREP
     if [ x$? == x0 ] ;then
         echo "projector idea already exit succ"
         break
